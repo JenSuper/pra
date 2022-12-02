c431df59b0d42e2e322a8ef432e42e8f01a42a0d
JenSuper <JenSuper@163.com>
2022-12-02 14:29:07 +0800

    feat:add list tools

---
+++ src/main/java/com/jensuper/prc/util/ListTool.java
@@ -0,0 +1,72 @@
+package com.jensuper.prc.util;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ *
+ */
+public class ListTool {
+    /**
+     * 函数式接口 T -> bollean
+     * @param keyExtractor
+     * @param <T>
+     * @return
+     */
+    public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        ConcurrentHashMap<Object, Boolean> map = new ConcurrentHashMap<>(16);
+        return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+
+    public static void removeNullElemnet(List list) {
+        Iterator iterator = list.iterator();
+        while(iterator.hasNext()){
+            if(Objects.isNull(iterator.next())){
+                iterator.remove();
+            }
+        }
+    }
+
+    /**
+     * @Description:    数组转字符串
+     * @author vincent@bigonelab.com
+     * @CreateDate:     2019/4/19 13:23
+     * @UpdateRemark:   修改内容
+     * @Version:        1.0
+     */
+    public static String list2String(List<String> data, String symbol){
+        if(CollectionUtils.isEmpty(data)){
+            return null;
+        }
+        return Joiner.on(symbol).join(data);
+    }
+
+    /**
+     * @Description:    字符串转数组
+     * @author vincent@bigonelab.com
+     * @CreateDate:     2019/4/19 13:23
+     * @UpdateRemark:   修改内容
+     * @Version:        1.0
+     */
+    public static List<String> string2List(String data, String symbol){
+        if(StringUtils.isBlank(data))
+        {
+            return new ArrayList<>();
+        }
+        return Lists.newArrayList(Splitter.on(symbol)
+                .trimResults()
+                .omitEmptyStrings()
+                .split(data));
+    }
+}
